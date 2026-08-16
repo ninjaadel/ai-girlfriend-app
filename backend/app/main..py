f61d906b41686133321fb5902e29a30ed90b4f75
ninjaadel <iurchenkoadel@gmail.com>
2026-08-16 23:44:37 +0300

    backend oluşturmaya devam

--- backend/app/main..py
+++ backend/app/main..py
@@ -3,3 +3,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleWare
 from pydantic import BaseModel
 from typing import List 
+
+
+app = FastAPI(title = "AI Girlfriend API")
+
+
+app.add_middleware(CORSMiddleWare,
+                   allow_origins=["*"])
